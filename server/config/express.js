52772d3e93ceb6c9d1968b3b649617fb5068bc4d
Sudhakar Reddy <sudhakarsjce@gmail.com>
2015-08-19 18:34:21 -0700

    some minor tweaks

--- server/config/express.js
+++ server/config/express.js
@@ -15,6 +15,8 @@ var errorHandler = require('errorhandler');
 var path = require('path');
 var config = require('./environment');
 
+var cacheTime = 86400000*7;     // 7 days
+
 module.exports = function(app) {
   var env = app.get('env');
 
@@ -29,7 +31,7 @@ module.exports = function(app) {
   
   if ('production' === env) {
     app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
-    app.use(express.static(path.join(config.root, 'public')));
+    app.use(express.static(path.join(config.root, 'public'), {maxAge:cacheTime}));
     app.set('appPath', config.root + '/public');
     app.use(morgan('dev'));
   }
@@ -37,7 +39,7 @@ module.exports = function(app) {
   if ('development' === env || 'test' === env) {
     app.use(require('connect-livereload')());
     app.use(express.static(path.join(config.root, '.tmp')));
-    app.use(express.static(path.join(config.root, 'client')));
+    app.use(express.static(path.join(config.root, 'client'), {maxAge:cacheTime}));
     app.set('appPath', 'client');
     app.use(morgan('dev'));
     app.use(errorHandler()); // Error handler - has to be last
